edd887c0f00d19998910a67d777990c043792f6e
Keymil72 <kpodlecki7@gmail.com>
2024-04-16 18:14:07 +0200

    #

--- src/Features/DataBase.js
+++ src/Features/DataBase.js
@@ -4,20 +4,27 @@ const { dbHost, dbUser, dbPassword } = require('../config.json');
 const Logger = require('./Logger');
 
 // funkcja łącząca z bazą danych i wykonująca zapytanie sql oraz przekazująca wynik do callbacka
-function polacz(sqlQuery, interaction, cb){
+function polacz(sqlQuery, interactionOrClient, cb){
     let con = Mysql.createConnection({
         host: dbHost,
         user: dbUser,
         password: dbPassword
     });
 
+    let client;
+    try {
+        client = interactionOrClient.client;
+    } catch (e){
+        client = interactionOrClient;
+    }
+
     con.connect(function (err) {
         if (err) throw err;
         con.query(sqlQuery, function (err, result, fields) {
             if (err) throw err;
             // po wykonaniu zapytania przekazuje wynik do callbacka
-            Logger.log(interaction.client, `Wykonano zapytanie: ${sqlQuery}`, 'dev Database');
-            cb(result, interaction);
+            Logger.log(client, `Wykonano zapytanie: ${sqlQuery}`, 'dev Database');
+            cb(result, interactionOrClient);
         });
     });
 }
