d4e0a6f58d80ee95bcd042cddd9f7d376f98be98
Keymil72 <kpodlecki7@gmail.com>
2024-05-29 19:14:24 +0200

    #

--- src/Commands/Utility/Watek.js
+++ src/Commands/Utility/Watek.js
@@ -1,6 +1,7 @@
 // FIXME dokończyć
 const { SlashCommandBuilder } = require("discord.js");
 
+const DataBase = require('../../Features/DataBase.js');
 const Logger = require('../../Features/Logger.js');
 
 module.exports = {
@@ -9,11 +10,12 @@ module.exports = {
         .setDescription('Obsługuje wątki z zadaniami użytkowników')
         .addSubcommand(subcommand => subcommand
             .setName('usun-osobe')
-            .setDescription('Usuwa osobę z twojego wątku'))
-        .addUserOption(option => option
-            .setName('osoba')
-            .setDescription('Osoba do usunięcia')
-            .setRequired(true))
+            .setDescription('Usuwa osobę z twojego wątku')
+            .addUserOption(option => option
+                .setName('osoba')
+                .setDescription('Osoba do usunięcia')
+                .setRequired(true))
+        )
         .addSubcommand(subcommand => subcommand
             .setName('pomoc')
             .setDescription('Pomoc do wątków')),
@@ -21,10 +23,38 @@ module.exports = {
     async execute(interaction) {
         // deklaracja stałych
         const client = interaction.client;
-        const ms = Math.abs(Date.now() - interaction.createdTimestamp);
-        // wyświetlenie aktualnego pingu bota
-        await interaction.reply({ content: `STYXXX dopłynął z opóźnieniem ${ms}ms.`, ephemeral: true });
-        //NOTE - Logger done
-        Logger.log(client, `Sprawdzono opóźnienie Styxxx'u - ${ms}ms`, __filename);
+        const subcommand = interaction.options.getSubcommand();
+        switch (subcommand) {
+            case 'usun-osobe':
+                removeUser(interaction);
+                break;
+            case 'pomoc':
+                //NOTE - Logger done
+                Logger.log(client, `Użytkownik ${owner.username} próbuje uzyskać pomoc w wątkach`, __filename);
+                break;
+        }
     },
 };
+
+async function removeUser(interaction) {
+    const owner = interaction.user;
+    const target = interaction.options.getUser('osoba');
+    const channel = interaction.channel;
+    let query = `SELECT watekId FROM StyxxxDcBot.Uzytkownicy WHERE uzytkownikId = ${owner.id}`;
+    if (target.id == interaction.client.application.id) {
+        await interaction.reply({ content: "Czy ty próbujesz wyrzucić Hermesa?!", ephemeral: true });
+        return;
+    } else if (target.id == owner.id) {
+        await interaction.reply({ content: "Czy ty próbujesz sam się wyrzucić?!", ephemeral: true });
+        return;
+    }
+    //NOTE - Logger done
+    Logger.log(client, `Użytkownik ${owner.username} próbuje usunąć osobę ${target.username} z wątku`, __filename);
+
+    DataBase.polacz(query, interaction, async (result) => {
+        if (channel.id == result[0].watekId) {
+            //TODO - Przepiąć na features/Watek.usun...
+            await interaction.reply({ content: `Wyrzucono użytkownika ${target.displayName} z twojego wątku`, ephemeral: true });
+        }
+    });
+}
