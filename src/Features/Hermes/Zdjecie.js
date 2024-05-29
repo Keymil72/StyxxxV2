d33fe9e7839a2f147c2b2c99d87ad75159e64e16
Keymil72 <kpodlecki7@gmail.com>
2024-05-29 02:10:16 +0200

    #

--- src/Features/Hermes/Zdjecie.js
+++ src/Features/Hermes/Zdjecie.js
@@ -3,14 +3,11 @@ const isImageURL = require('image-url-validator').default;
 
 // sprawdza czy string jest linkiem do zdjęcia
 async function sprawdz(string) {
-    console.log(`1: ${string}`);
     if (string == null) return null;
     // sprawdza czy link jest poprawny
     if (Link.sprawdz(string) != null) {
-        console.log(`2: ${string}`);
         // sprawdza czy link jest zdjęciem
         let url = await isImageURL(string) ? string : null;
-        console.log(`3: ${url}`);
         return url;
     }
     // zwraca null jeśli link nie jest zdjęciem
