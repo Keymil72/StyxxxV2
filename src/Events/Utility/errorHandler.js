523e51edb8d9c632fb65015323eee6d7245d4251
Keymil72 <kpodlecki7@gmail.com>
2024-10-01 18:08:10 +0200

    #

--- src/Events/Utility/errorHandler.js
+++ src/Events/Utility/errorHandler.js
@@ -1,7 +1,7 @@
 const { Events } = require('discord.js');
 
 const Logger = require('../../Features/Logger.js');
-const index = require('../../index.js');
+const Index = require('../../index.js');
 
 //poprawić clienta 
 module.exports = {
@@ -12,5 +12,6 @@ module.exports = {
         let errorMessage = error.toString();
 		//NOTE - Logger done
         Logger.log(client, errorMessage, __filename, "error");
+		Index.start();
 	},
 };
