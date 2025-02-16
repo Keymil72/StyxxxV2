06c7710dd5b2e7e9920acec79911bf2c33610d1a
Keymil72 <kpodlecki7@gmail.com>
2025-02-16 23:13:14 +0100

    #

--- src/Features/Hermes/Link.js
+++ src/Features/Hermes/Link.js
@@ -9,15 +9,40 @@ function sprawdz(link){
         }
         // REVIEW możliwie coś poknociłem z sprawdzeniem linku !!! (chyba new URL(link) powinien być w if'ie)!!!
         new URL(link);
-        //NOTE - Logger done
-        Logger.log(null, `Link "${link}" jest poprawny`, __filename);
-        return link;
+        if (sprawdzLink(link))
+        {
+            //NOTE - Logger done
+            Logger.log(null, `Link "${link}" jest poprawny`, __filename);
+            return link;
+        }
+        else
+        {
+            //NOTE - Logger done
+            Logger.log(null, `Link "${link}" jest niepoprawny`, __filename);
+            console.log(`Link jest niepoprawny: ${link}`);
+            return null;
+        }
     } catch (error) {
         //NOTE - Logger done
         Logger.log(null, `Error przy sprawdzaniu linku "${link}" - ${error}`, __filename);
         return null;
     }
 }
+
+function sprawdzLink(link){
+    var request = new XMLHttpRequest();  
+    request.open('GET', link, true);
+    request.onreadystatechange = function(){
+    if (request.readyState === 4){
+        if (request.status === 404) {  
+            return false;
+        }  
+    }
+    return true;
+};
+request.send();
+}
+
 
 // eksportuje funkcje
 module.exports = { sprawdz };
